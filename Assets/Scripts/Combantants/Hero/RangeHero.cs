26892377682805321d4c93d16e8c2ca71f694aa4
Can Aydin <canaydingithub@gmail.com>
2024-09-21 22:22:07 +0300

    Imported  Arrow from an asset, Ranger  now attacks via sending an  arrow

--- Assets/Scripts/Combantants/Hero/RangeHero.cs
+++ Assets/Scripts/Combantants/Hero/RangeHero.cs
@@ -1,21 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class RangeHero : Hero
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform _arrowSpawnPos;
 
-    void Update()
-    {
-    }
-
+    private const int ArrowPoolId = 1;
+    
     public override void Attack(Combantant target)
     {
-        Debug.Log($"Ranger  Attacked: {target}");
+        var targetHealthController = target.GetComponent<IHealthController>();
+        SpawnAndShootArrow(target.transform, () =>
+        {
+            targetHealthController.TakeDamage((int)AttackPower);
+            base.Attack(target);
+            
+        });
+    }
 
+    private void SpawnAndShootArrow(Transform target, Action OnMoveComplete)
+    {
+        var arrowObj = ObjectPool.Instance.SpawnFromPool(ArrowPoolId, _arrowSpawnPos.position, Quaternion.identity);
+        var arrowTransform = arrowObj.transform;
+        var targetPos = target.position;
+        targetPos.y = 2;
+        arrowTransform.DOMove(targetPos, 1f).OnComplete(() =>
+        {
+           OnMoveComplete?.Invoke(); 
+           arrowObj.SetActive(false);
+        });
     }
 }
